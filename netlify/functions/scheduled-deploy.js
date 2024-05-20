f939cb14440e5fc7bb81a09f3abe9c92ba59614e
Joe Crawford <joe@artlung.com>
2024-05-19 23:15:22 -0700

    Daily Runs

---
+++ netlify/functions/scheduled-deploy.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch')
+
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/664ae8fa0d17a85f6f5c2e18'
+
+const handler = schedule('1 0 * * *', async () => {
+    await fetch(BUILD_HOOK, {
+        method: 'POST'
+    }).then(response => {
+        console.log('Build hook response:', response)
+    })
+
+    return {
+        statusCode: 200
+    }
+})
+
+export { handler }
